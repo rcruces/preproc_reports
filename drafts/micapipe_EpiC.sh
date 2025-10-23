3be0134d345f12bf72fc392ee6ea6b81b0385862
rcruces <raulrcruces@gmail.com>
2025-10-23 14:07:37 -0400

    updated gitignore & update EpiC script to run on micapipe v0.2.3

--- drafts/micapipe_EpiC.sh
+++ drafts/micapipe_EpiC.sh
@@ -22,26 +22,54 @@ img_singularity=/data/mica1/01_programs/micapipe-v0.2.0/micapipe_"${version}".si
 bids=/data_/mica3/BIDS_EpiC/rawdata
 fs_lic=/data_/mica1/01_programs/freesurfer-7.3.2/license.txt
 out=/data_/mica3/BIDS_EpiC/derivatives
-threads=15
-tmpDir=/tmp
+threads=10
+tmpDir=/data_/mica2/tmpDir
 
 # Create command string
-command="singularity run --writable-tmpfs --containall -B ${bids}:/bids -B ${out}:/out -B ${tmpDir}:/tmp -B ${fs_lic}:/opt/licence.txt ${img_singularity}"
+command="singularity run --writable-tmpfs --containall -B ${bids}:/bids -B ${out}:/out -B ${tmpDir}:/tmpdir -B ${fs_lic}:/opt/licence.txt ${img_singularity}"
 
+# ------------------------------------------------------------------
+# proc_dwi & SC
+
+# cleanup
+${command} \
+-bids /bids -out /out -fs_licence /opt/licence.txt -threads ${threads} -sub ${sub} -ses ${ses} \
+-proc_dwi -SC -acqStr acq-b2000 -cleanup
+
+# reprocess
+${command} \
+-bids /bids -out /out -fs_licence /opt/licence.txt -threads ${threads} -sub ${sub} -ses ${ses} \
+-proc_dwi -SC -dwi_acq b2000 -tmpDir /tmpdir -regSynth -dwi_upsample \
+-dwi_main /bids/sub-${sub}/ses-${ses}/dwi/sub-${sub}_ses-${ses}_acq-b2000_dir-AP_dwi.nii.gz
+
+
+# ------------------------------------------------------------------
+# All modules
+# ${command} \
+# -bids /bids -out /out -fs_licence /opt/licence.txt -threads ${threads} -sub ${sub} -ses ${ses} \
+    # -proc_surf -surf_dir /out/freesurfer/${sub}_${ses} \
+    # -post_structural -freesurfer \
+    # -proc_flair \
+    # -GD \
+    # -regSynth \
+    # -proc_dwi \
+    # -SC
+
+# ------------------------------------------------------------------
 # func cleanup
-${command} \
--bids /bids -out /out -fs_licence /opt/licence.txt -threads ${threads} -sub ${sub} -ses ${ses} \
--proc_func -acqStr desc-se_task-rest_bold -cleanup
+# ${command} \
+# -bids /bids -out /out -fs_licence /opt/licence.txt -threads ${threads} -sub ${sub} -ses ${ses} \
+# -proc_func -acqStr desc-se_task-rest_bold -cleanup
 
-${command} \
--bids /bids -out /out -fs_licence /opt/licence.txt -threads ${threads} -sub ${sub} -ses ${ses} \
--proc_func -acqStr desc-se_task-sternberg_bold -cleanup
+# ${command} \
+# -bids /bids -out /out -fs_licence /opt/licence.txt -threads ${threads} -sub ${sub} -ses ${ses} \
+# -proc_func -acqStr desc-se_task-sternberg_bold -cleanup
 
 # Run func
-${command} \
--bids /bids -out /out -fs_licence /opt/licence.txt -threads ${threads} -sub ${sub} -ses ${ses} \
--proc_func -mainScanStr task-rest_bold -tmpDir ${tmpDir} -noFIX -NSR
+# ${command} \
+# -bids /bids -out /out -fs_licence /opt/licence.txt -threads ${threads} -sub ${sub} -ses ${ses} \
+# -proc_func -mainScanStr task-rest_bold -tmpDir ${tmpDir} -noFIX -NSR
 
-${command} \
--bids /bids -out /out -fs_licence /opt/licence.txt -threads ${threads} -sub ${sub} -ses ${ses} \
--proc_func -mainScanStr task-sternberg_bold -tmpDir ${tmpDir} -noFIX -NSR
+# ${command} \
+# -bids /bids -out /out -fs_licence /opt/licence.txt -threads ${threads} -sub ${sub} -ses ${ses} \
+# -proc_func -mainScanStr task-sternberg_bold -tmpDir ${tmpDir} -noFIX -NSR
